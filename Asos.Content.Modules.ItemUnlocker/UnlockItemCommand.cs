ee8a6ddd44e072d491f9f4e4192603b690900a5d
Jason Cox <jason.cox@asos.com>
2016-11-29 15:19:32 +0000

    Adding item unlocker command

---
+++ Asos.Content.Modules.ItemUnlocker/UnlockItemCommand.cs
@@ -0,0 +1,60 @@
+﻿namespace Asos.Content.Modules.ItemUnlocker
+{
+    using System;
+
+    using Sitecore;
+    using Sitecore.Diagnostics;
+    using Sitecore.SecurityModel;
+    using Sitecore.Shell.Framework.Commands;
+
+    [Serializable]
+    public class UnlockItemCommand : Command
+    {
+        private const string UnlockRole = "sitecore\\Asos Unlock Other Users Items";
+
+        public override void Execute(CommandContext context)
+        {
+            var item = context.Items[0];
+
+            if (!item.Access.CanWriteLanguage() || !item.Locking.IsLocked())
+            {
+                return;
+            }
+            var user = Context.User;
+
+            Log.Info(string.Format("{0} has unlocked {1}", user.Name, AuditFormatter.FormatItem(item)), this);
+
+            using (new SecurityDisabler())
+            {
+                item.Editing.BeginEdit();
+                item.Locking.Unlock();
+                item.Editing.EndEdit();
+                Context.ClientPage.SendMessage(this, "item:checkedin");
+            }
+        }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            if (!Context.User.IsInRole(UnlockRole))
+            {
+                return CommandState.Disabled;
+            }
+
+            var item = context.Items[0];
+
+            if (!item.Access.CanWriteLanguage())
+            {
+                return CommandState.Disabled;
+            }
+
+            var itemIsLocked = item.Locking.IsLocked() && !item.Locking.HasLock();
+
+            if (!itemIsLocked)
+            {
+                return CommandState.Disabled;
+            }
+
+            return CommandState.Enabled;
+        }
+    }
+}
